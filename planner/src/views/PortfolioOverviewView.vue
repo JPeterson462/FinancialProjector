9db4c6567fd768c3f6e9387e5e55a3f2a2f2cf43
Jonathan Peterson <jpeterson462@gmail.com>
2026-01-01 14:18:27 -0500

    Got tabbed view working

--- planner/src/views/PortfolioOverviewView.vue
+++ planner/src/views/PortfolioOverviewView.vue
@@ -3,13 +3,27 @@
     <h1 style="padding-bottom: 20px; width: 100%; border-bottom: 1px solid blue">Overview</h1>
     <br>
     <div class="container">
-      <div style="width: 140px">
-        <span class="portfolio-tab selected-tab">Accounts</span>
-        <span class="portfolio-tab">Balances</span>
-        <span class="portfolio-tab">Tax Forms</span>
-        <span class="portfolio-tab">Budget</span>
-      </div>
-      <div class="col">
+      <div class="row">
+        <div style="width: 160px">
+          <span class="portfolio-tab" :class="{ 'selected-tab': selectedTab == ACCOUNTS }" @click="selectTab(ACCOUNTS)">Accounts</span>
+          <span class="portfolio-tab" :class="{ 'selected-tab': selectedTab == BALANCES }" @click="selectTab(BALANCES)">Balances</span>
+          <span class="portfolio-tab" :class="{ 'selected-tab': selectedTab == TAX_FORMS }" @click="selectTab(TAX_FORMS)">Tax Forms</span>
+          <span class="portfolio-tab" :class="{ 'selected-tab': selectedTab == BUDGET }" @click="selectTab(BUDGET)">Budget</span>
+        </div>
+        <div class="col">
+          <span class="tab-content" :class="{ 'tab-hidden': selectedTab != ACCOUNTS }">
+            <h2>Accounts</h2>
+          </span>
+          <span class="tab-content" :class="{ 'tab-hidden': selectedTab != BALANCES }">
+            <h2>Balances</h2>
+          </span>
+          <span class="tab-content" :class="{ 'tab-hidden': selectedTab != TAX_FORMS }">
+            <h2>Tax Forms</h2>
+          </span>
+          <span class="tab-content" :class="{ 'tab-hidden': selectedTab != BUDGET }">
+            <h2>Budget</h2>
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -18,10 +32,26 @@
 <script>
 export default {
   props: ['portfolioJson'],
+  data() {
+    return {
+      selectedTab: null,
+
+      ACCOUNTS: 0,
+      BALANCES: 1,
+      TAX_FORMS: 2,
+      BUDGET: 3
+    };
+  },
   mounted() {
-    this.selectedTab = 'accounts';
+    this.selectTab(this.ACCOUNTS)
     var portfolio = JSON.parse(window.history.state.portfolioJson);
     console.log(portfolio);
+  },
+  methods: {
+    selectTab(tabId) {
+      console.log("selecting tab " + tabId);
+      this.selectedTab = tabId;
+    }
   }
 };
 </script>
